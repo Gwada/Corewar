3bda9fb721c19d3c8d5307da6b6e2dc6eb15f719
Dimitri LAVAURY-COLLOT <dlavaury@e3r2p23.42.fr>
2018-06-10 13:34:13 +0200

    vm done

--- srcs/vm/srcs/display_corewar.c
+++ srcs/vm/srcs/display_corewar.c
@@ -2,52 +2,62 @@
 #include "time.h"
 #include "../../libft/includes/ft_printf.h"
 
-void		put_header(t_core *c, t_process *p, unsigned char opc)
+void		put_header(t_core *c, t_process *p, unsigned char opc, int i_1)
 {
-	char	h[1024];
-	int		i_1;
-	int		i_2;
+	char	h[512];
 
-	i_1 = 0;
-	i_2 = 0;
-	ft_bzero(h, 1024);
-
-	i_1 += ft_psprintf(&h[i_1], "{bold}cycle:\t\t[{magenta}");//
+	ft_bzero(h, 512);
+	i_1 += ft_psprintf(&h[i_1], "{bold}{yellow}DISPLAY_COREWAR STATES\n");//
+	i_1 += ft_psprintf(&h[i_1], "{eoc}{bold}cycle:\t\t[{magenta}");//
 	i_1 += ft_psprintf(&h[i_1], "%5d{eoc}{bold}]\t", c->total_cycle);//
 	i_1 += ft_psprintf(&h[i_1], "current_cycle:\t[{green}");//
 	i_1 += ft_psprintf(&h[i_1], "%4d{eoc}{bold}]\t", c->current_cycle);//
 	i_1 += ft_psprintf(&h[i_1], "current_live:\t[{green}");//
 	i_1 += ft_psprintf(&h[i_1], "%5d{eoc}{bold}]\t", c->current_cycle_live);
+	i_1 += ft_psprintf(&h[i_1], "cycle_to_die:\t[{green}");
+	i_1 += ft_psprintf(&h[i_1], "%4d{eoc}{bold}]\t", c->max_cycle);//
+	i_1 += ft_psprintf(&h[i_1], "last_decr:\t[{red}");
+	i_1 += ft_psprintf(&h[i_1], "%2d{eoc}{bold}]\n", c->last_decr);//
+	i_1 += ft_psprintf(&h[i_1], "n_process:\t[{magenta}");
+	i_1 += ft_psprintf(&h[i_1], "%5d{eoc}{bold}]\t", c->n_process);//
+	i_1 += ft_psprintf(&h[i_1], "p->pc:\t\t[{yellow}");
+	i_1 += ft_psprintf(&h[i_1], "%4u{eoc}{bold}]\t", p->pc);//
+	i_1 += ft_psprintf(&h[i_1], "opc:\t\t[{yellow}");
+	i_1 += ft_psprintf(&h[i_1], "%5hhu{eoc}{bold}]\n\n{eoc}", opc);//
+	ft_printf("%s", h);
+//	ft_printf("%s\nsize: %d\n", h, i_1);//
+}
 
-	i_1 += ft_psprintf(&h[i_1], "cycle_to_die:\t[%4d]\t{red}last_decr:\t[%2d]\nn_process:\t[%5d]\t{yellow}p->pc:\t\t[%4u]\topc:\t\t[%2hhu]\n\n{eoc}", c->current_cycle_live, c->max_cycle, c->last_decr, c->n_process, p->pc, opc);//
-//	i_1 += ft_psprintf(&h[i_1], "{bold}{magenta}cycle:\t\t[%5d]\t{green}current_cycle:\t[%4d]\t{green}current_live:\t[%5d]\tcycle_to_die:\t[%4d]\t{red}last_decr:\t[%2d]\nn_process:\t[%5d]\t{yellow}p->pc:\t\t[%4u]\topc:\t\t[%2hhu]\n\n{eoc}", c->total_cycle, c->current_cycle, c->current_cycle_live, c->max_cycle, c->last_decr, c->n_process, p->pc, opc);//
-		ft_printf("%s\n", h);
-/*		for (int i = 1; i <= 16; ++i)//
-		{//
-			i_2 += ft_printf("reg[%2u] = {magenta}%10#x{eoc}\t", i, p->reg[i]);//
-			!(i % 8) ? i_2 += ft_printf("\n") : 0;//
-		}//
-*/		ft_printf("i_1 = %d\ti_2 = %d\n", i_1, i_2);
+void		put_process(t_process *p, int i_1, int reg)
+{
+	char	h[1024];
 
-
+	ft_bzero(h, 1024);
+	i_1 += ft_psprintf(&h[i_1], "{bold}{yellow}PROCESS STATES{eoc}\n");//
+	while (++reg < 17)
+	{
+		i_1 += ft_psprintf(&h[i_1], "{bold}reg[%2u] = ", reg);//
+		i_1 += ft_psprintf(&h[i_1], "{magenta}%10#x{eoc}\t", p->reg[reg]);//
+		!(reg % 8) ? i_1 += ft_psprintf(&h[i_1], "\n") : 0;//
+	}
+//	i_1 += ft_psprintf(&h[i_1], "\nprocess name: %s\n", p->ins.description);//
+//	ft_printf("%s", h);
+	ft_printf("%s\nsize: %d\n", h, i_1);//
 }
 
 void		display_cw(t_core *c, t_process *p, unsigned char opc, int state)
 {
 	if (!state)
 	{
-//		system("clear");//
-
-		ft_printf("{bold}{yellow}IN\tDISPLAY_COREWAR part 1\n{eoc}");//
-		put_header(c, p, opc);
-		ft_printf("{bold}{yellow}END\tDISPLAY_COREWAR part 1\n\n{eoc}");//
-
+		system("clear");//
+		put_header(c, p, opc, 0);
+		put_process(p, 0, 0);
 	}
 	else if (state == 1)
 	{
 //		ft_printf("{bold}{yellow}\nIN\tDISPLAY_COREWAR part 2\n{eoc}");//
 	//	ft_print_mem(&c->ram, MEM_SIZE, 64, 0);//
-	//	nanosleep((const struct timespec[]){{0, 100000000L}}, NULL);//
+		nanosleep((const struct timespec[]){{0, 100000000L}}, NULL);//
 
 //		ft_printf("{bold}{yellow}END\tDISPLAY_COREWAR part 2\n{eoc}");//
 
