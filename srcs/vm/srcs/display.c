fa379711aba4cad3a0a84daed3cb3ae259fc077e
Dimitri LAVAURY-COLLOT <dlavaury@e3r2p23.42.fr>
2018-06-13 15:54:15 +0200

    vm done 2.0

--- srcs/vm/srcs/display.c
+++ srcs/vm/srcs/display.c
@@ -6,7 +6,7 @@
 /*   By: dlavaury <dlavaury@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/01 14:05:10 by dlavaury          #+#    #+#             */
-/*   Updated: 2018/06/13 15:42:38 by dlavaury         ###   ########.fr       */
+/*   Updated: 2018/06/13 15:53:57 by dlavaury         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,22 @@
 void		display_usage(char *name)
 {
 	ft_printf("{bold}Usage: %s [-dump [nbr_cycles] -v]", name);
+	ft_printf(" [[-Debug[start]] champion1.cor]");
 	ft_printf(" [[-n [number]] champion1.cor]");
 	ft_printf(" [[-n [number]] champion2.cor] ...{eoc}\n");
 	ft_printf("{underline}-dump [nbr_cycles]{runderline}:");
-	ft_printf(" Dumps memory after N cycles then exits.\n");
+	ft_printf(" Dumps memory after nbr_cycles cycles then exits.\n");
 	ft_printf("Value is initialized to null by default.\n");
 	ft_printf("\tnbr_cycles: must be strictly positive and ");
 	ft_printf("can not be bigger than %d.\n", IMAX);
+	ft_printf("{underline}-Debug[start]{eoc}:");
+	ft_printf(" Show RAM and process status with general states");
+	ft_printf(", in a graphical interface.\n");
+	ft_printf("\tstart: show the RAM after start cycles. ");
+	ft_printf("Value is initialized to null by default.\n");
 	ft_printf("{underline}-v{eoc}:");
 	ft_printf(" Show RAM status in a graphical interface.\n");
-	ft_printf("{underline}-p [number]{eoc}: Assign id N to the next champion.");
+	ft_printf("{underline}-n [number]{eoc}: Assign id N to the next champion.");
 	ft_printf(" If this value is not initialized,\n");
 	ft_printf("the machine chooses the first possible value.\n");
 	ft_printf("\tnumber: must be strictly positive and ");
